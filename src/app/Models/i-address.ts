835e1bc865cc407687d915f53ca0be8aaf942f32
Mauro-Cassoni <mauro.cassoni.94@gmail.com>
2024-03-06 14:43:43 +0100

    interface

---
+++ src/app/Models/i-address.ts
@@ -0,0 +1,13 @@
+export interface IAddress {
+  id : number;
+  firstname : string;
+  lastname : string;
+  street : string;
+  streetNumber : string;
+  city : string;
+  postalCode : number;
+  country : string;
+  province : string;
+  municipality : string;
+  phoneNumber : string;
+}
